6b6190eab3294cdb7dcea140543d5b33c8e1ebe8
QuangNhat1 <quangnhat180799@gmail.com>
2021-05-12 16:51:59 +0700

    first commit

---
+++ 18048271_LeQuangNhat_Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SendingData {
+    TextView txtTotal ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtTotal = findViewById(R.id.txtTotal);
+
+
+    }
+
+    @Override
+    public void sendData(String data) {
+        int d1= Integer.parseInt(data) * 148000;
+        txtTotal.setText(String.valueOf(d1)+" đ");
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout,BelowFragment.newInstance(String.valueOf(d1)+" đ")).commit();
+
+
+
+
+    }
+
+}
